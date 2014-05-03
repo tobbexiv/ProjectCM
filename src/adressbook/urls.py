75b5f5f3f207bf3cb3c147e987e2030653716a7c
AHaberl <andreashaberl@gmx.net>
2014-05-03 14:31:44 +0200

    added crud for adressbook use case

--- src/adressbook/urls.py
+++ src/adressbook/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-	url(r'^$', 'adressbook.views.home', name='home'),
+	url(r'^$', 'adressbook.views.adress_list', name='adress_list'),
+  	url(r'^new$', 'adressbook.views.adress_create', name='adress_new'),
+  	url(r'^edit/(?P<pk>\d+)$', 'adressbook.views.adress_update', name='adress_edit'),
+  	url(r'^delete/(?P<pk>\d+)$', 'adressbook.views.adress_delete', name='adress_delete'),
 )
