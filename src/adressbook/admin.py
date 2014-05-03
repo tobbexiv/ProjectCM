9cde512be87f9d7911120516c124c1986cb4e15d
AHaberl <andreashaberl@gmx.net>
2014-05-03 13:16:48 +0200

    added adressbook model

---
+++ src/adressbook/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+
+#------------------------------------------------
+## For test use only:
+
+#from .models import Adress
+#
+#class AdressbookAdmin(admin.ModelAdmin):
+#	class Meta:
+#		model = Adress
+
+#admin.site.register(Adress, AdressbookAdmin)
+
+#------------------------------------------------
