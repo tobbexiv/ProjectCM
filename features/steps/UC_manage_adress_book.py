67b99ac5d077bafe8ca884352a72d2e5d6f279ba
AHaberl <andreashaberl@gmx.net>
2014-05-06 13:25:18 +0200

    renamed css, added behavior

---
+++ features/steps/UC_manage_adress_book.py
@@ -0,0 +1,36 @@
+@given('I am logged in')
+def step_impl(context):
+    assert False
+
+@given('I am on the contact list')
+def step_impl(context):
+    assert False
+
+@given('I clicked create contact')
+def step_impl(context):
+    assert False
+
+@when('I type in the correct contact data')
+def step_impl(context):
+    assert False
+
+@when('I upload a profile picture for the contact')
+def step_impl(context):
+    assert False
+
+@when('I click create')
+def step_impl(context):
+    assert False
+
+@then('I should see a confirmation message')
+def step_impl(context):
+    assert False
+
+@given('I clicked delete contact')
+def step_impl(context):
+    assert False
+
+@when('I confirm the deletion')
+def step_impl(context):
+    assert False
+
