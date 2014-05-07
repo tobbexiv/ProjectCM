723f440ce4018c570b6d7fa60eb0c2e9c2dd1d39
Tobias Engelmann <T.O.B.B.E@web.de>
2014-05-07 12:11:18 +0200

    basic step definition

--- features/steps/UC_manage_adress_book.py
+++ features/steps/UC_manage_adress_book.py
@@ -2,37 +2,68 @@ from behave import *
 
 @given('I am logged in')
 def step_impl(context):
-	assert True
+    assert False
 
 @given('I am on the contact list')
 def step_impl(context):
-	assert True
+    assert False
 
 @given('I clicked create contact')
 def step_impl(context):
-	assert True
+    assert False
 
 @when('I type in the correct contact data')
 def step_impl(context):
-	assert True
+    assert False
 
 @when('I upload a profile picture for the contact')
 def step_impl(context):
-	assert True
+    assert False
 
 @when('I click create')
 def step_impl(context):
-	assert True
+    assert False
 
 @then('I should see a confirmation message')
 def step_impl(context):
-	assert True
+    assert False
 
 @given('I clicked delete contact')
 def step_impl(context):
-	assert True
+    assert False
 
 @when('I confirm the deletion')
 def step_impl(context):
-	assert True
+    assert False
 
+@given('I clicked edit for a contact')
+def step_impl(context):
+    assert False
+
+@when('I change the data correctly')
+def step_impl(context):
+    assert False
+
+@given('I clicked show for a contact')
+def step_impl(context):
+    assert False
+
+@when('I click back')
+def step_impl(context):
+    assert False
+
+@then('I should see the list of contacts')
+def step_impl(context):
+    assert False
+
+@when('I click abort')
+def step_impl(context):
+    assert False
+
+@when('I type in false data')
+def step_impl(context):
+    assert False
+
+@then('I should see an error message')
+def step_impl(context):
+    assert False
