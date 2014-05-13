891cdeea9988345221b26e91a7c9ce7ccbf36d82
AHaberl <andreashaberl@gmx.net>
2014-05-14 00:12:08 +0200

    deleted deprecated app

--- src/userdata/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.conf.urls import patterns, url
-
-# urlpatterns = patterns('', 
-# 	url(r'^$', 'userdata.views.userdata_show', name='userdata_show'),
-# 	url(r'^edit/(?P<pk>\d+)$', 'userdata.views.userdata_edit', name='userdata_edit'),
-	
-# )
