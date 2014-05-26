eda64fbaf87a865c056689d1760e80345ca0f730
AHaberl <andreashaberl@gmx.net>
2014-05-26 13:11:05 +0200

    added base template

--- src/projectcm/views.py
+++ src/projectcm/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, render_to_response, RequestContext
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 @login_required
 def home(request):
-	
-	return render_to_response("index.html", locals(), context_instance=RequestContext(request))
+	data = {}
+	data['title'] = "ProjectCM Main Page"
+
+	return render(request,'index.html' , data)
+
