5a4e040e37469a85a11b8ee9abc6a6388430e1d5
AHaberl <andreashaberl@gmx.net>
2014-06-15 11:02:02 -0700

    added urls for appointments

--- src/cal/urls.py
+++ src/cal/urls.py
@@ -7,6 +7,10 @@ urlpatterns = patterns('',
         url(r'^edit/(?P<pk>\d+)$', 'cal.views.calendar_update', name='calendar_update'),
         url(r'^delete/(?P<pk>\d+)$', 'cal.views.calendar_delete', name='calendar_delete'),
 	url(r'^appointment/list$', 'cal.views.appointment_list', name='appointment_view'),
+	url(r'^appointment/create$', 'cal.views.appointment_create', name='appointment_create'),
+	url(r'^appointment/updte/(P<pk>\+)$', 'cal.views.appointment_update', name='appointment_update'),
+	url(r'^appointment/delete/(P<pk>\+)$', 'cal.views.appointment_delete', name='appointment_delete'),
+
 
 )
 
