9cb721917d622573ddedc4cd23d07d4c4d416bb4
AHaberl <andreashaberl@gmx.net>
2014-06-16 10:33:10 -0700

    added json responses

--- src/cal/views.py
+++ src/cal/views.py
@@ -46,7 +46,13 @@ def calendar_create(request):
 		calendar.calendar_owner = request.user
 		calendar.save()
 
-		return HttpResponse("data saved")
+		response = {}
+		response['userName'] = request.user.username
+		response['success'] = True
+		response['data'] = 'Calendar successfully created'
+
+		json_response = json.dumps(response)
+		return HttpResponse(json_response, content_type="application/json")
 
 	return render(request, 'generic_form.html', {'form':form})
 	
@@ -58,7 +64,13 @@ def calendar_update(request, pk, template_name='generic_form.html'):
         form = CalendarForm(request.POST or None, instance=calendar)
         if form.is_valid() and request.method == "POST" and request.is_ajax:
                 form.save()
-                return HttpResponse("data saved")
+                response = {}
+				response['userName'] = request.user.username
+				response['success'] = True
+				response['data'] = 'Calendar successfully updated'
+
+				json_response = json.dumps(response)
+				return HttpResponse(json_response, content_type="application/json")
 
         return render(request, template_name, {'form':form})
 
@@ -67,7 +79,13 @@ def calendar_delete(request, pk, template_name='cal/calendar_delete.html'):
         calendar = get_object_or_404(Calendar, pk=pk)
         if request.method=='POST' and request.is_ajax:
                 calendar.delete()
-                return HttpResponse("data deleted")
+                response = {}
+				response['userName'] = request.user.username
+				response['success'] = True
+				response['data'] = 'Calendar successfully deleted'
+
+				json_response = json.dumps(response)
+				return HttpResponse(json_response, content_type="application/json")
 
         return render(request, template_name, {'object':calendar})
 
@@ -79,48 +97,49 @@ def appointment_list(request):
 		#now = datetime.utcnow().replace(tzinfo=utc)
 		from_date = dateparser.parse(json_data['fetch_after_date'])#now - timedelta(days=7) #
 		to_date = dateparser.parse(json_data['fetch_before_date'])#now + timedelta(days=7) #
-		calendar = Calendar.objects.get(pk=json_data['calendar']) #json_data['calendar']
+		calendar_list = Calendar.objects.filter(calendar_owner=request.user) #json_data['calendar']
 		
 		ser = CalSerializer()
 		all_appointments = []
 
-		appointments = Appointment.objects.filter(calendar=calendar, series=None, start_date__lte=to_date, end_date__gte=from_date)
-		for appo in appointments:
-			all_appointments.append(json.dumps(appo, cls=DateTimeEncoder))	
+		for calendar in calendar_list:
+			appointments = Appointment.objects.filter(calendar=calendar, series=None, start_date__lte=to_date, end_date__gte=from_date)
+			for appo in appointments:
+				all_appointments.append(json.dumps(appo, cls=DateTimeEncoder))	
 
 
-		series = Series.objects.filter(first_occurence__lte=to_date, last_occurence__gte=to_date)
+			series = Series.objects.filter(first_occurence__lte=to_date, last_occurence__gte=to_date)
+			
+			for occ in series:
+				ser_apps = Appointment.objects.get(series=occ)
+				all_appointments.append(json.dumps(ser_apps, cls=DateTimeEncoder))
+				day1 = (from_date - timedelta(days=from_date.weekday()))
+				day2 = (to_date - timedelta(days=to_date.weekday()))
+
+				if occ.reoccurences == 'daily':
+					days_between = int((day2 - day1).days)
+					
+					for i in range(1, days_between):
+						new_start_date = ser_apps.start_date + timedelta(days=1*i)
+						new_end_date = ser_apps.end_date + timedelta(days=1*i)
+						new_app = Appointment(calendar=calendar, title=ser_apps.title, description=ser_apps.description, start_date=new_start_date, end_date=new_end_date)
+						
+						all_appointments.append(json.dumps(new_app, cls=DateTimeEncoder))
 		
-		for occ in series:
-			ser_apps = Appointment.objects.get(series=occ)
-			all_appointments.append(json.dumps(ser_apps, cls=DateTimeEncoder))
-			day1 = (from_date - timedelta(days=from_date.weekday()))
-			day2 = (to_date - timedelta(days=to_date.weekday()))
-
-			if occ.reoccurences == 'daily':
-				days_between = int((day2 - day1).days)
-				
-				for i in range(1, days_between):
-					new_start_date = ser_apps.start_date + timedelta(days=1*i)
-					new_end_date = ser_apps.end_date + timedelta(days=1*i)
-					new_app = Appointment(calendar=calendar, title=ser_apps.title, description=ser_apps.description, start_date=new_start_date, end_date=new_end_date)
+				elif occ.reoccurences == 'weekly':
+					weeks_between = int((day2 - day1).days / 7)
 					
-					all_appointments.append(json.dumps(new_app, cls=DateTimeEncoder))
-	
-			elif occ.reoccurences == 'weekly':
-				weeks_between = int((day2 - day1).days / 7)
-				
-				for i in range(1, weeks_between):
-					new_start_date = ser_apps.start_date + timedelta(days=7*i) 
-					new_end_date = ser_apps.end_date + timedelta(days=7*i)
-					new_app = Appointment(calendar=calendar, title=ser_apps.title, description=ser_apps.description, start_date=new_start_date, end_date=new_end_date)
+					for i in range(1, weeks_between):
+						new_start_date = ser_apps.start_date + timedelta(days=7*i) 
+						new_end_date = ser_apps.end_date + timedelta(days=7*i)
+						new_app = Appointment(calendar=calendar, title=ser_apps.title, description=ser_apps.description, start_date=new_start_date, end_date=new_end_date)
 
-					all_appointments.append(json.dumps(new_app, cls=DateTimeEncoder))
+						all_appointments.append(json.dumps(new_app, cls=DateTimeEncoder))
 
+						
 					
-				
-			elif occ.reoccurences == 'monthly':
-				pass		
+				elif occ.reoccurences == 'monthly':
+					pass		
 
 
 
@@ -141,7 +160,13 @@ def appointment_create(request):
 	if form.is_valid() and request.method == "POST" and request.is_ajax:
 		appointment = form.save()
 
-		return HttpResponse("data saved")
+		response = {}
+		response['userName'] = request.user.username
+		response['success'] = True
+		response['data'] = 'Appointment successfully created'
+
+		json_response = json.dumps(response)
+		return HttpResponse(json_response, content_type="application/json")
 
 	return render(request, 'cal/appointment_form.html')
 
@@ -151,7 +176,13 @@ def appointment_update(request, pk, template_name='generic_form.html'):
 	form = AppointmentForm(rquest.POST or None, instance=appointment)
 	if form.is_valid() and request.method == "POST" and request.is_ajax:
 		form.save()
-		return HttpRepsonse("data saved")
+		response = {}
+		response['userName'] = request.user.username
+		response['success'] = True
+		response['data'] = 'Appointment successfully updated'
+
+		json_response = json.dumps(response)
+		return HttpResponse(json_response, content_type="application/json")
 
 	return render(request, template_name, {'form':form})
 	
@@ -161,7 +192,13 @@ def appointment_delete(request, pk, template_name='appointment_delete'):
 	appointment = get_object_or_404(Appointment, pk=pk)
 	if request.method == "POST" and request.is_ajax:
 		appointment.delete()
-		return HttpResponse("data deleted")
+		response = {}
+		response['userName'] = request.user.username
+		response['success'] = True
+		response['data'] = 'Appointment successfully deleted'
+
+		json_response = json.dumps(response)
+		return HttpResponse(json_response, content_type="application/json")
 
 	return render(request, template_name, {'object':appointment})	
 
@@ -176,7 +213,14 @@ def series_create(request, template_name='cal/series_create.html'):
 		appointment.series = series
 		appointment.save()
 
-		return HttpResponse("data saved")
+		response = {}
+		response['userName'] = request.user.username
+		response['success'] = True
+		response['data'] = 'Series successfully created'
+
+		json_response = json.dumps(response)
+		return HttpResponse(json_response, content_type="application/json")
+		
 
 	data = {}
 	data['appointment'] = appointment_form
@@ -192,7 +236,13 @@ def series_delete(request, pk, template_name='cal/series_delete.html'):
 	if request.method == 'POST' and request.is_ajax:
 		appointment.delete()
 		series.delete()
-		return HttpResponse('data deleted')
+		response = {}
+		response['userName'] = request.user.username
+		response['success'] = True
+		response['data'] = 'Series successfully deleted'
+
+		json_response = json.dumps(response)
+		return HttpResponse(json_response, content_type="application/json")
 
 	data = {}
 	data['appointment'] = appointment
