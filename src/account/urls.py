072df3e51ee7fe209205b8457cc28f1344dd56d6
Tobias Engelmann <T.O.B.B.E@web.de>
2014-06-17 01:50:02 +0200

    changes on login template

--- src/account/urls.py
+++ src/account/urls.py
@@ -4,10 +4,8 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('', 
 	url(r'^$', 'account.views.account_show'),
 	url(r'^edit/(?P<pk>\d+)$', 'account.views.account_edit', name='account_edit'),
-	url(r'^login/?$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}),
+	url(r'^login/?$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}, name='login'),
 	url(r'^logout/?$', 'django.contrib.auth.views.logout', {'next_page' : 'projectcm.views.home'}), 
-	#url(r'^register/$', 'accounts.forms.RegistrationView.as_view()', name='registration_register'),
-	
 	url(r'^password/change/?$', 'django.contrib.auth.views.password_change', name='password_change'),
 	url(r'^password/change/done/?$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
 )
