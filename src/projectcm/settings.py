f7f4eae8564de23d628d878a9ee7e6ea5eef4379
Tobias Engelmann <T.O.B.B.E@web.de>
2014-06-29 23:14:45 +0200

    fixed static error

--- src/projectcm/settings.py
+++ src/projectcm/settings.py
@@ -87,6 +87,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "files") 
